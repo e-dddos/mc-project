b0267cbed686be0a7673f878d7310c35fa305f7c
KaiFrueh <kai.fruehwald@haw-hamburg.de>
2024-12-10 12:35:01 +0100

    Update calc.c

--- calc.c
+++ calc.c
@@ -1,6 +1,7 @@
 #include "calc.h"
 
-double speed(double wheelDiameter, double frequency)
+double speed(double frequency)
 {
+  wheel diameter = 5; //test value as 5
   return wheelDiameter * frequency;
 }
