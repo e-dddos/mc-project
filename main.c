61de5c647cb084895ba454778378f1c86aa81d88
Eduard Lomtadze <eduard.lomtadze@haw-hamburg.de>
2025-01-02 16:11:53 +0100

    removed old comment

--- main.c
+++ main.c
@@ -1,11 +1,3 @@
-// Testprogram two LCD Displays 480x272 and 800x480 RGB
-// Display controller Type SSD 1963 => Solomon Systech
-// Initialisation + Single Pixel and Rectangle Output
-// ARM-Contoller Clock not changed ... to be tested
-// Based partially of tests and sources from Ole Roenna 2020
-// V0.1-V0.4 K.R. Riemschneider .. 1-4 Oct 2020
-
-
 #include "tacho.h"
 
 void main(void)
